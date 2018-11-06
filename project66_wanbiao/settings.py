adc21b3616cf3a5d9caa80f2ba99570ab6e5aa68
Riderdmyf <duanmuyf@qq.com>
2018-11-06 14:04:58 +0800

    create database

--- project66_wanbiao/settings.py
+++ project66_wanbiao/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project66_wanbiao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Wanbiao',
+        'USER': 'root',
+        'PASSWORD': 'dmyf5210',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
@@ -121,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, )
