bafa4dff23c80e88310c01b75fd20527c0d630a1
Riderdmyf <duanmuyf@qq.com>
2018-11-09 09:54:25 +0800

    add name namespace

--- project66_wanbiao/urls.py
+++ project66_wanbiao/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^', include('wanbiao.urls')),
+    url(r'^', include('wanbiao.urls', namespace='wanbiao')),
 ]
--- wanbiao/urls.py
+++ wanbiao/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import url
 from wanbiao import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^cart/$', views.cart),
-    url(r'^mobile/$', views.mobile),
-    url(r'^creditcard/$', views.creditcard),
-    url(r'^service/$', views.service),
-    url(r'^login/$', views.login),
-    url(r'^register/$', views.register),
-    url(r'^epos/$', views.epos), #buy page
+    url(r'^$', views.index, name='index'),
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^mobile/$', views.mobile, name='mobile'),
+    url(r'^creditcard/$', views.creditcard, name='creditcard'),
+    url(r'^service/$', views.service, name='service'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^epos/$', views.epos, name='epos'), #buy page
 
     url(r'^addwatch/$', views.addwatch),
 ]
