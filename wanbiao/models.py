0090b9f7c660007418ec8dc6f630c971af385cf2
Riderdmyf <duanmuyf@qq.com>
2018-11-10 10:59:49 +0800

    add models Base Wheel

--- wanbiao/models.py
+++ wanbiao/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Watch(models.Model):
     img = models.CharField(max_length=100)
+
+class Base(models.Model):
+    img = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+class Wheel(Base):
+    pass
